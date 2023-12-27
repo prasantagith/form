dc85793dda74a6390cea023ddee0906d4ede2746
prasantagith <prasantasethi2002gmail.com>
2023-12-27 21:24:21 +0530

    On branch main Your branch is up to date with 'origin/main'. 	new file:   app/migrations/0001_initial.py 	new file:   app/migrations/__pycache__/0001_initial.cpython-312.pyc

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Topics(models.Model):
+    Tname=models.CharField(max_length=100,primary_key=True)
+    
+    def __str__(self):
+        return self.Tname
--- projectCC/urls.py
+++ projectCC/urls.py
@@ -20,5 +20,5 @@ from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/',index,name='index')
+    path('index/',index,name='index'),
 ]
